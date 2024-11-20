ebb0babd32fb6a87fe56f41445d3dddfedb35adf
Sam Liburd <sam.liburd@rcot.co.uk>
2024-11-20 11:55:13 +0000

    added clarity

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Clarity from "@microsoft/clarity";
 import './index.scss'
 import App from './App.tsx'
+
+const projectId = "p1i02u0ezu"
+
+Clarity.init(projectId);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
